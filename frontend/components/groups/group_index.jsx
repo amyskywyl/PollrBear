dc61de24ca00be3f4eb3021d7c826585b2e49a25
Yilin Wang <amyskywyl@gmail.com>
2019-01-30 02:22:40 -0500

    tue night more

--- frontend/components/groups/group_index.jsx
+++ frontend/components/groups/group_index.jsx
@@ -22,8 +22,29 @@ class GroupIndex extends React.Component {
 
     return (
       <div className="poll-body">
-        <Link className="create-poll" to="/questions/new">Create</Link>
-
+        <div className="app-discovery">
+          <Link className="create-poll" to="/questions/new">Create</Link>
+          <div className="app-discovery--title">
+          Download the app
+          </div>
+          <p>
+          Use the  app to embed polls in your slides.
+          </p>
+          <p>
+          Select your presentation software to start the download.
+          </p>
+          <ul>
+            <li>
+              <div className="icon app-discovery--icon" data-glyph="powerpoint-logo-gray"></div>
+            </li>
+            <li>
+              <div className="icon app-discovery--icon" data-glyph="keynote-logo-gray"></div>
+            </li>
+            <li>
+              <div className="icon app-discovery--icon" data-glyph="googleslides-logo-gray"></div>
+            </li>
+          </ul>
+        </div>
         <ul className="poll-groups">
           {groups}
         </ul>
